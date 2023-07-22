d3e6a281a58b3f4deedc5696648f0b5f367300c3
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-07-22 18:49:30 -0400

    put join game form into its own component

--- src/components/JoinGameForm.js
+++ src/components/JoinGameForm.js
@@ -1,29 +1,36 @@
 import React, { useState } from 'react';
 
 const JoinGameForm = ({ onJoin }) => {
-    const [name, setName] = useState('');
-    const [avatar, setAvatar] = useState('');
+  const [name, setName] = useState('');
+  const [avatar, setAvatar] = useState(''); //maybe use an image
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        onJoin({ name, avatar });
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onJoin({ name, avatar });
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Name:
-                <input type="text" value={name} onChange={e => setName(e.target.value)} />
-            </label>
-            <label>
-                Avatar:
-                <select value={avatar} onChange={e => setAvatar(e.target.value)}>
-                    {/* Add options for each available avatar */}
-                </select>
-            </label>
-            <button type="submit">Join Game</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          placeholder="Enter username"
+          // update the username textfield as user is typing
+          onChange={e => setName(e.target.value)} />
+      </label>
+      <label>
+        Avatar:
+        <select
+          value={avatar}
+          onChange={e => setAvatar(e.target.value)}>
+          {/* Add options for each available avatar */}
+        </select>
+      </label>
+      <button type="submit">Join Game</button>
+    </form>
+  );
 };
 
 export default JoinGameForm;
--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,37 +1,22 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import JoinGameForm from '../components/JoinGameForm';
 import '../styles.css'
 
 const Join = ({ onJoin }) => {
-  const [username, setUsername] = useState('');
-  const navigate = useNavigate(); // for navigation
-
-  //when a player joins
-  //go to /game page
+  
+  const navigate = useNavigate(); // for navigation with react router
   const handleJoin = (event) => {
-    event.preventDefault(); 
-    onJoin({ name: username, avatar: '' }); //save username to the app
+    //onJoin is passed as a prop from the app.js component
+    //will pass the username up to the app component to save across app
+    onJoin(event);
     navigate('/game');  //go to game screen
-  };
-
-  // update the username textfield as user is typing
-  const handleChange = (event) => {
-    setUsername(event.target.value);
   };
 
   return (
     <div>
-      {/* todo: put this in JoinGameForm component */}
-      <form onSubmit={handleJoin}>
-        <input
-          type="text"
-          placeholder="Enter username"
-          value={username}
-          onChange={handleChange}
-        />
-        <button type="submit">Join Game</button>
-      </form>
+      <JoinGameForm onJoin={handleJoin} />
+      <h1>Below is where the instructions will go</h1>
+      <p>instructions</p>
     </div>
   );
 };
