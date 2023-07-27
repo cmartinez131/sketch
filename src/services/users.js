ac6cbff39b34cc08c325efff20044e81ac7eca5f
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-07-27 16:36:28 -0400

    services

---
+++ src/services/users.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const baseUrl = '/api/users'
+
+const getAll = () => {
+    return axios.get(baseUrl)
+}
+
+const getById = id => {
+    return axios.get(`${baseUrl}/${id}`)
+}
+
+const create = newUser => {
+    return axios.post(baseUrl, newUser)
+}
+
+const update = (id, updatedUser) => {
+    return axios.put(`${baseUrl}/${id}`, updatedUser)
+}
+
+const remove = id => {
+    return axios.delete(`${baseUrl}/${id}`)
+}
+
+export default { getAll, getById, create, update, remove }
