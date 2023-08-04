50374f2fa3c9cc22c064f96c09ac1f4f5e986edc
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-08-03 20:31:47 -0400

    make it so that only drawer can draw

--- src/components/Drawingboard.js
+++ src/components/Drawingboard.js
@@ -2,8 +2,12 @@ import React, { useRef, useEffect, useState } from 'react';
 import { io } from 'socket.io-client'
 import '../styles.css'
 
+//TODO: manage permission state so that client doesn't have to ask permission on every
+//mouse move. Create event listeners and socket conenctions so server can update
+//permission states when there is a new word
+
 //define DrawingBoard component
-const DrawingBoard = ({ socket }) => {
+const DrawingBoard = ({ socket, player }) => {
     //use the react useRef hook to get a reference to the canvas DOM element
     const canvasRef = useRef(null);
 
@@ -12,12 +16,26 @@ const DrawingBoard = ({ socket }) => {
     const [color, setColor] = useState('#000000');
     const [width, setWidth] = useState(10);
     const [mode, setMode] = useState('draw');  // New state variable for the mode
+    const [permissionState, setPermissionState] = useState(null)
 
     //define function to handle mousedown event: emit 'start-drawing' event to server and all clients
-    const handleMouseDown = (event) => {    //event is the object passed into function
-        setIsDrawing(true);
-        draw(event);
-        socket.emit('start-drawing', { clientX: event.clientX, clientY: event.clientY, color, width })
+    const handleMouseDown = (event) => {    
+        //ask permission from server before player can initiate drawing
+        socket.emit('request-permission', { username: player.name });
+
+        socket.on('permission-response', ({ hasPermission }) => {
+            if (hasPermission) {
+                setIsDrawing(true);
+                const canvas = canvasRef.current;
+                const ctx = canvas.getContext('2d');
+                // set the new starting point for line
+                ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+                draw(event);
+                socket.emit('start-drawing', { clientX: event.clientX, clientY: event.clientY, color, width })
+            } else {
+                console.log('It is not your turn to draw')
+            }
+        })
     }
 
     //define function to handle mouse-up event: emit 'stop=drawing' event
@@ -40,12 +58,12 @@ const DrawingBoard = ({ socket }) => {
     //define function to draw on the canvas
     const draw = (mouseEvent) => {
         const canvas = canvasRef.current;
-        //ctx is the variable that refers to the 2d rendering context for canvas
+        //c tx is the variable that refers to the 2d rendering context for canvas
         const ctx = canvas.getContext('2d');
         ctx.lineWidth = width;
-        // Set stroke color. If we're in 'erase' mode, use the white
-        ctx.strokeStyle = mode === 'draw' ? color : '#FFFFFF';  // Use the background color when erasing
-        //draw line to point where the mouse is
+        // set stroke color. If we're in 'erase' mode, use the white
+        ctx.strokeStyle = mode === 'draw' ? color : '#FFFFFF';
+        // draw line to point where the mouse is
         ctx.lineTo(mouseEvent.clientX - canvas.offsetLeft, mouseEvent.clientY - canvas.offsetTop);
         ctx.stroke();//apply the stroke
     }
